b42391d5b4a79ebafcdb8a96f81c41ee602733fc
akhila-ariyachandra <akhila_ariyachandra@live.com>
2017-05-11 10:38:38 +0530

    Added logger (bunyan)

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var restify = require('restify');
 var config = require('./config');
-var app = restify.createServer({name:'mfin-api'});
- 
+var bunyan = require('bunyan');
+
+var log = bunyan.createLogger({
+    name: 'MFin',
+    streams: [{
+        type: 'rotating-file',
+        path: 'logs/MFin.log',
+        period: '1d',   // daily rotation
+        count: 3        // keep 3 back copies
+    }]
+});
+
+var app = restify.createServer({
+	name: 'mfin-api',
+	log: log
+});
+
 app.use(restify.fullResponse());
 app.use(restify.bodyParser());
 app.use(restify.queryParser());
