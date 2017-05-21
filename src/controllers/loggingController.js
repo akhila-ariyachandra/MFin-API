3370bb6e9798587fee93636bdb836af072223a75
akhila-ariyachandra <akhila_ariyachandra@live.com>
2017-05-21 11:25:04 +0530

    [User Stories 11 and 21] Updated /viewLogs to read multiple log files at once

--- src/controllers/loggingController.js
+++ src/controllers/loggingController.js
@@ -1,16 +1,23 @@
-//This Controller deals with all functionalities of Customer
+//This Controller deals with all functionalities of Logging
  
 function loggingController () {
 	const fs = require('fs');
+	const async = require('async');
+
+	const dirPath = './logs/';
 
 	// View Logs
 	this.viewLogs = function(req, res){
-		var data = '';
-		const readStream = fs.createReadStream('./logs/MFin.log', 'utf8');
-		readStream.on('data', function(stream){
-			data += stream;
-		}).on('end', function(){
-			res.send({'logs':data});
+		fs.readdir(dirPath, function (err, filesPath) {
+    		if (err) throw err;
+    		filesPath = filesPath.map(function(filePath){	//generating paths to file
+        		return dirPath + filePath;
+    		});
+    		async.map(filesPath, function(filePath, cb){ //reading files or dir
+        		fs.readFile(filePath, 'utf8', cb);
+    		}, function(err, results) {
+        		res.send(results);
+    		});
 		});
 	};
 	 
