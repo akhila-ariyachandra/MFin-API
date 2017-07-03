5db1e5838669b5fe29ae1bebce6c4e24a49ef422
akhila-ariyachandra <akhila_ariyachandra@live.com>
2017-07-03 22:45:10 +0530

    Added morgan logger (replaced bunyan)

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ var bodyParser = require('body-parser');
 var config = require('config');
 var morgan = require('morgan');
 var jwt = require('jsonwebtoken');
+var fs = require('fs');
+var path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// create a write stream (in append mode) 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+// use morgan to log requests to the log file
+app.use(morgan('combined', {stream: accessLogStream}));
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
