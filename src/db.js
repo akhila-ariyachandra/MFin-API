72c3cc0af5d264daba87faac037360cfc81b14a2
akhila-ariyachandra <akhila_ariyachandra@live.com>
2017-07-12 18:56:40 +0530

    Use local test database for automated testing

--- src/db.js
+++ src/db.js
@@ -4,7 +4,21 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var config = require('config');
 
-mongoose.connect(config.dbPath);
+// Use mockgoose for automated testing
+// Mockgoose is for a temporary DB stored in RAM
+var Mockgoose = require('mockgoose').Mockgoose;
+var mockgoose = new Mockgoose(mongoose);
+
+if (process.env.NODE_ENV === 'test') {
+	// While testing use mockgoose
+	mockgoose.prepareStorage().then(function () {
+		mongoose.connect(config.dbPath);
+	});
+} else {
+	// Else use the web database
+	mongoose.connect(config.dbPath);
+}
+
 var db = mongoose.connection;
 
 db.on('error', function () {
