6f1bb1391925ab56cb49df85217e5c6c69f464fb
akhila-ariyachandra <akhila_ariyachandra@live.com>
2017-10-05 21:44:37 +0530

    Added check for account type in authorization middleware
    Signed-off-by: akhila-ariyachandra <akhila_ariyachandra@live.com>

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -2,9 +2,14 @@
 
 module.exports = (req, res, next) => {
     // Dependencies
+    const accessPermissions = require("config").accessPermissions
     const app = require("../app").app
     const jwt = require("jsonwebtoken")
     const tokenCache = require("../app").tokenCache
+
+    // Get permissions
+    const route = req.method + " " + req.route.path
+    const permissions = accessPermissions.find(a => a.route === route)
 
     // Check header or url parameters or post parameters for token
     const token = req.headers["x-access-token"]
@@ -25,12 +30,14 @@ module.exports = (req, res, next) => {
         } else if (token) {
             // Verifies secret and checks exp
             jwt.verify(token, app.get("superSecret"), (err, decoded) => {
-                if (err) {
+                if (err || !permissions[decoded._doc.accountType]) {
+                    /* If error decoding token or the permission for account type 
+                    is set to false */
                     return res.status(401).send({
                         success: false,
                         message: "Unauthorised"
                     })
-                } else {
+                } else if (permissions[decoded._doc.accountType]) {
                     // If everything is good, save to request for use in other routes
                     req.decoded = decoded
                     return next()
